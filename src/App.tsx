59ab18265b2f65e4a1c003fb970cf299ae6efdd9
Zeynep Tekin <tknzeynep3@gmail.com>
2025-08-26 14:52:47 +0300

    düzenlendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Navigate'i import et
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import QuizPage from './pages/QuizPage';
-import Layout from './components/Layout'; // Layout'u import ediyoruz
+import Layout from './components/Layout';
 
 function App() {
   return (
@@ -13,6 +13,9 @@ function App() {
         {/* Tüm sayfaları Layout bileşeniyle sarıyoruz */}
         <Route path="/" element={<Layout />}>
         
+          {/* Ana sayfa (/) için login'e yönlendirme */}
+          <Route index element={<Navigate to="/login" />} />
+
           {/* Herkese Açık Rotalar */}
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
