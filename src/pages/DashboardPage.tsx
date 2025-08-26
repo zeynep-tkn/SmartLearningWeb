4ce481bdffef0ec3c549a42ab7706661fbadf00c
Zeynep Tekin <tknzeynep3@gmail.com>
2025-08-26 14:39:06 +0300

    düzenlendi

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, JSX } from 'react';
 import axios from 'axios';
 import { getToken } from '../auth';
 import { useNavigate } from 'react-router-dom';
